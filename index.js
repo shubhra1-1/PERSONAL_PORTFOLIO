e7596c664de19640aaa0f864c73d6d1c0ce2c84a
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2023-08-26 18:48:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Frontend-developer","Photographer","Web-developer", "Content-creator"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:800,
+    loop:true
+});
